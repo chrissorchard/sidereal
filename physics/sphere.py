c71b462f18f7bb64f2842ddc4bb8ffc048041f56
Jack Edge <yellowbounder@gmail.com>
2011-02-23 23:02:36 +0000

    sphere.py - More tweaking, bullet experimentation
    Trying to understand the underlying bullet library, and rotation and
    pitch and raw, and other such things.

--- physics/sphere.py
+++ physics/sphere.py
@@ -1,16 +1,19 @@
-import bullet
+import time
 
-class Ball(object):
-    def __init__(self,origin=bullet.Vector3(0,10,0)):
+import bullet
+from bullet import Vector3
+
+class Ball(bullet.RigidBody,object):
+    def __init__(self,origin=bullet.Vector3(0,100,0)):
         self.shape = bullet.SphereShape(1.0)
         self.transform = bullet.Transform()
         self.transform.setIdentity()
         self.transform.setOrigin(origin)
         self.motion = bullet.DefaultMotionState()
         self.motion.setWorldTransform(self.transform)
-        self.body = bullet.RigidBody(self.motion, self.shape, 1.0)
+        bullet.RigidBody.__init__(self,self.motion, self.shape, 1.0)
 
-class Ground(object):
+class Ground(bullet.RigidBody,object):
     def __init__(self):
         self.shape = bullet.BoxShape(bullet.Vector3(50,50,50))
         self.transform = bullet.Transform()
@@ -18,8 +21,30 @@ class Ground(object):
         self.transform.setOrigin(bullet.Vector3(0,-50,0))
         self.motion = bullet.DefaultMotionState()
         self.motion.setWorldTransform(self.transform)
-        self.body = bullet.RigidBody(self.motion, self.shape)
+        bullet.RigidBody.__init__(self,self.motion, self.shape)
+
+class PhysicsHelper(object):
+    def __init__(self,body):
+        self.body = body
+
+    def _yaw(self,yaw):
+        pass
+    def _pitch(self,pitch):
+        pass
+    def _roll(self,roll):
+        pass
+
+def _tick(world,items):
+    world.stepSimulation(1.0/60.0,100)
+    for obj in items:
+        vector = obj.getWorldTransform().getOrigin()
+        print vector
 
 if __name__=='__main__':
     b = Ball()
+    b.setRestitution(0.9)
     g = Ground()
+    world = bullet.DiscreteDynamicsWorld()
+
+    world.addRigidBody(g)
+    world.addRigidBody(b)
