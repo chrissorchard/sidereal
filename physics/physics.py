2d79aece27d4c631bf109ac4dbd686661b144a17
Jack Edge <yellowbounder@gmail.com>
2011-02-25 15:57:55 +0000

    physics.py - Psuedo code for FlightHelper.tick()
    Described the process of the FlightHelper's tick method, which should
    orient a gameobject towards its list of waypoints.

--- physics/physics.py
+++ physics/physics.py
@@ -28,4 +28,34 @@ class FlightHelper(list):
     """
     def __init__(self):
         super().__init__(self)
+    def tick(self):
+        # This method should be called on each Physics tick
+
+        # Do we have any waypoints? If not, then return
+        if len(self) == 0:
+            return
         
+        # Given the waypoint at the beginning of the list
+        waypoint = self[0] 
+        
+        # Determine what we have to do to orient ourselves towards
+        # this waypoint.
+
+        # Are we spinning enough?
+
+        # Are we spinning too much?
+
+        # Are we spinning just right?
+
+        # Use any methods of rotation that we have. Some may be more
+        # extreme than others, which we only use in life or death situations
+
+        # If we're pointing towards it enough, then fire the relevent
+        # or whatever mechanism we use for movement
+
+        # But don't go too fast, because we have to slow down. We could
+        # go fast in case of an emergency.
+
+        # It's likely that different set of methods will have to be used
+        # for a ship that doesn't use rear engines + gimal or rotation
+        # thrusters
