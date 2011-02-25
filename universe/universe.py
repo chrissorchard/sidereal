65eb9ffd9d4bcdf707e90a4180eab09135259b31
Jack Edge <yellowbounder@gmail.com>
2011-02-25 16:52:10 +0000

    universe.py - Added method to get unique IDs
    The universe module (or class) now has a method that gives out unique
    ids.
    
    I'm pretty sure that it's not thread safe, and in face, it might
    make sense for universe, if it only consists of lists, and other utility
    functions, to be used as a Cython module.

--- universe/universe.py
+++ universe/universe.py
@@ -3,15 +3,19 @@ in which all ingame object occupy. It should keep track of all in-game
 related objects, such as ships, explosions, projectiles, and so on, or be
 able to keep track of them on demand."""
 
-__all__ = ["Universe","foo","bar"]
+__all__ = ["Universe","foo","bar","get_unique_id"]
 
 class Universe(object):
     def __init__(self):
-        pass
+        self._id = 0
     def foo(self):
         pass
     def bar(self):
         pass
+    def get_unique_id(self):
+        id = self._id
+        self.id += 1
+        return id
 
 # inspired by python's random module, create one instance, and export its
 # functions as module-level functions. The user can create their own
@@ -19,3 +23,4 @@ class Universe(object):
 _inst = Universe()
 foo = _inst.foo
 bar = _inst.bar
+get_unique_id = _inst.get_unique_id
