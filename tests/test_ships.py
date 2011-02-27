9be1ee037186556272f377709618d276382df5f6
Jack Edge <yellowbounder@gmail.com>
2011-02-27 18:17:21 +0000

    Created basic JSON ship creation unittest
    Created a basic test, to make sure that the ship can be created given
    some ship JSON.

--- tests/test_ships.py
+++ tests/test_ships.py
@@ -2,6 +2,16 @@ import unittest
 
 import sidereal.ships
 
+class ShipCreationTestCase(unittest.TestCase):
+    def test_json_creation(self):
+        # Tests the ship creation
+        ship = sidereal.ships.Ship.create_from_json(sidereal.ships._examplejson)
+        self.assertEqual(ship.mass,5000)
+
+_loader = unittest.TestLoader()
+suite = _loader.loadTestsFromTestCase(ShipCreationTestCase)
+
+
 if __name__=='__main__':
     unittest.main()
 
