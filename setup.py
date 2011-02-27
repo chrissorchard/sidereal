ab3052ad22847a2c0c9a5b2302ee1940e140f1e8
Jack Edge <yellowbounder@gmail.com>
2011-02-27 16:44:44 +0000

    Created basic distutils setup.py
    For now, our project doesn't need anything too complicated. As such,
    this just installs the files in the "sidereal" package.
    
    What could be done in the future is the ability to deduce the VERSION
    from git tags, or some magic like that.

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='sidereal',
+      version='0.0.1',
+      description='A 3D Real Time Strategy game in space',
+      author='Sidereal Dev Team',
+      url='https://github.com/coiax/sidereal',
+      packages=['sidereal'],
+     )
+
