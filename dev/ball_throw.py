0607475d93e25e63840e995479b63e260ef8f724
Jack Edge <yellowbounder@gmail.com>
2011-03-12 21:24:41 +0000

    New: ball_throw.py, test of physics + turtles
    Added a new folder, called ./dev for random scripts that are more of use
    by developers, but not actual unit and/or other tests.
    
    This one creates a number of random spheres, and throws them in the air,
    and displays the result with sidereal.turtles.

---
+++ dev/ball_throw.py
@@ -0,0 +1,50 @@
+"""A test script, that checks that basic physics is working, by
+"throwing" a ball, represented by the default turtle display.
+
+"""
+import time
+import random
+import argparse
+
+parser = argparse.ArgumentParser(description="Throw some turtle shaped balls.")
+parser.add_argument('--fixed-seed','-S',action='store_true',default=False)
+parser.add_argument('--ball-number','-n',type=int,default=3)
+args = parser.parse_args()
+
+if args.fixed_seed:
+    random.seed(100)
+
+
+import sidereal.physics.odeobjects as odeobjects
+import sidereal.turtles as turtles
+
+d = turtles.Display()
+d.autocoord = True
+world = odeobjects.PhysicsWorld()
+
+world.setGravity((0,-9.81,0))
+for i in range(args.ball_number):
+    ball = odeobjects.PhysicsObject(world)
+    ball.body.setPosition((random.randint(-100,100),random.randint(0,100),0))
+    ball.body.setLinearVel((random.randint(-30,50),random.randint(20,40),0))
+    d.add(ball)
+
+
+timetaken = 0
+while True:
+    timestamp = time.time()
+    d.draw()
+    elapsed = time.time() - timestamp
+    timetaken += elapsed
+    world.step(elapsed)
+    discard = []
+    for ball in d:
+        if ball.coord[1] <= 0:
+            discard.append(ball)
+
+    for ball in discard:
+        d.discard(ball)
+
+    if len(d) == 0:
+        break
+
