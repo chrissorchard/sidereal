1e9c8895b032dc95b6f9761f4cbba5c4e3bdb21b
Jack Edge <yellowbounder@gmail.com>
2011-04-10 00:50:53 +0100

    Moved DigestDict from server.py to network.py
    Cleaner this way.

---
+++ sidereal/network.py
@@ -0,0 +1,33 @@
+# random network utlity classes and functions
+
+class DigestDict(dict):
+    def digest(self):
+        # Given a dictionary, inserts a json digest, assuming that
+        # the element 'digest' contains N zeros, and it's printed
+        # with indent 4, sorted
+        # where N is the hash length
+        dcopy = self.copy()
+        hasher = hashlib.new('md5')
+        N = len(hasher.hexdigest())
+        dcopy['digest'] = "0"*N
+        
+        representation = json.dumps(dcopy,sort_keys=True,indent=4)
+        hasher.update(representation)
+        digest = hasher.hexdigest()
+
+        self['digest'] = digest
+
+    def verify(self):
+        our_digest = self.get('digest',None)
+
+        dcopy = self.copy()
+
+        hasher = hashlib.new('md5')
+        N = len(hasher.hexdigest())
+        dcopy['digest'] = "0"*N
+        
+        representation = json.dumps(dcopy,sort_keys=True,indent=4)
+        hasher.update(representation)
+        their_digest = hasher.hexdigest()
+
+        return our_digest == their_digest
--- sidereal/server.py
+++ sidereal/server.py
@@ -4,38 +4,7 @@ import sys
 
 from twisted.internet.protocol import DatagramProtocol
 
-class DigestDict(dict):
-    def digest(self):
-        # Given a dictionary, inserts a json digest, assuming that
-        # the element 'digest' contains N zeros, and it's printed
-        # with indent 4, sorted
-        # where N is the hash length
-        dcopy = self.copy()
-        hasher = hashlib.new('md5')
-        N = len(hasher.hexdigest())
-        dcopy['digest'] = "0"*N
-        
-        representation = json.dumps(dcopy,sort_keys=True,indent=4)
-        hasher.update(representation)
-        digest = hasher.hexdigest()
-
-        self['digest'] = digest
-
-    def verify(self):
-        our_digest = self.get('digest',None)
-
-        dcopy = self.copy()
-
-        hasher = hashlib.new('md5')
-        N = len(hasher.hexdigest())
-        dcopy['digest'] = "0"*N
-        
-        representation = json.dumps(dcopy,sort_keys=True,indent=4)
-        hasher.update(representation)
-        their_digest = hasher.hexdigest()
-
-        return our_digest == their_digest
-
+from sidereal.network import DigestDict
 
 class Server(object):
     port = 25005
