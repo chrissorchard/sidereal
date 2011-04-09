d39a9fe31a96da745c3bec8e90c1f3e0a0f38dee
Jack Edge <yellowbounder@gmail.com>
2011-04-09 19:42:43 +0100

    Added DigestDict code, for verification of JSON
    When we're sending our JSON across the network, we can use this function
    to ensure that nothing's been changed. This isn't security against
    malicious tampering, because frankly, md5 is completely broken.

--- sidereal/server.py
+++ sidereal/server.py
@@ -1,5 +1,41 @@
+import json
+import hashlib
+
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
+
+class DigestDict(dict):
+    def digest(self):
+        # Given a dictionary, inserts a json digest, assuming that
+        # the element 'digest' contains N zeros, and it's printed
+        # with indent 4, sorted
+        # where N is the hash length
+        dcopy = self.copy()
+        hasher = hashlib.new('md5')
+        N = len(hasher.hexdigest())
+        dcopy['digest'] = "0"*N
+        
+        representation = json.dumps(dcopy,sort_keys=True,indent=4)
+        hasher.update(representation)
+        digest = hasher.hexdigest()
+
+        self['digest'] = digest
+
+    def verify(self):
+        our_digest = self['digest']
+
+        dcopy = self.copy()
+
+        hasher = hashlib.new('md5')
+        N = len(hasher.hexdigest())
+        dcopy['digest'] = "0"*N
+        
+        representation = json.dumps(dcopy,sort_keys=True,indent=4)
+        hasher.update(representation)
+        their_digest = hasher.hexdigest()
+
+        return our_digest == their_digest
+
 
 class Echo(DatagramProtocol):
 
@@ -7,5 +43,5 @@ class Echo(DatagramProtocol):
         print "received %r from %s:%d" % (data, host, port)
         self.transport.write(data, (host, port))
 
-reactor.listenUDP(9999, Echo())
-reactor.run()
+#reactor.listenUDP(9999, Echo())
+#reactor.run()
