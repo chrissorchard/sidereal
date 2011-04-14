2e21958dd4cf0735c3dde38b9559f379483478af
Jack Edge <yellowbounder@gmail.com>
2011-04-14 20:50:23 +0100

    Additional gasau in servertest

--- dev/servertest.py
+++ dev/servertest.py
@@ -6,9 +6,14 @@ s.setup()
 import sidereal.universe as universe
 import sidereal.physics as physics
 
-gasau = universe.id()
-s.gamestate._physics[gasau] = body = physics.Body(s.gamestate._world)
-body.velocity = 0,1,0
+def new(velocity=(0,1,0)):
+    gasau = universe.id()
+    body = s.gamestate.new_body(gasau)
+    body.velocity = velocity
+
+new()
 
 from twisted.internet import reactor
+reactor.callLater(4,new,(3,-4,7))
+reactor.callLater(8,new,(9,-1.5,3))
 reactor.run()
