9aae3e2853ad5dd43278d72d6b7873ac992593a7
Jack Edge <yellowbounder@gmail.com>
2011-04-15 16:39:31 +0100

    Made pseudoheader class

--- sidereal/network.py
+++ sidereal/network.py
@@ -1,5 +1,6 @@
 import json
 import hashlib
+import struct
 
 # random network utlity classes and functions
 
@@ -41,3 +42,57 @@ class DigestDict(dict):
 
 class BadDigest(Exception):
     pass
+
+class BadLength(Exception):
+    pass
+
+class PseudoHeader(object):
+    format = ">H16sBH"
+    length = struct.calcsize(format)
+    """
+    our header consists of:
+
+    unsigned short - sequence number
+    16 byte string - md5 hash
+    unsigned short - flags
+    unsigned short - length of packet, header + payload
+    """
+
+    @classmethod
+    def calculate(cls,data,sequence=0,**kwargs):
+        # data should be a string, otherwise, convert it
+        data = str(data)
+
+        # do flags
+        flags = 0 | 1 | 4
+
+        length = cls.length + len(data)
+
+        packet = struct.pack(cls.format,sequence,"0"*16,flags,length)
+        hasher = hashlib.md5()
+        hasher.update(packet)
+        hash = hasher.digest()
+
+        return struct.pack(cls.format,sequence,hash,flags,length) + data
+
+    @classmethod
+    def unpack(cls,data,check_hash=True):
+        # given a packet, check that its length, and hash are true
+        sequence,hash,flags,length = struct.unpack(cls.format,data[:cls.length])
+
+        if len(data) != length:
+            raise BadLength("Packet specifies a size of {0}, we got {1}.".format(length,len(data)))
+
+        if check_hash:
+            verify_packet = struct.pack(cls.format,sequence,"0"*16,flags,length)
+            hasher = hashlib.md5()
+            hasher.update(verify_packet)
+            our_hash = hasher.digest()
+
+            if hash != our_hash:
+                raise BadDigest
+
+        return sequence,hash,flags,length,data[cls.length:]
+
+calculate_packet = PseudoHeader.calculate
+unpack_packet = PseudoHeader.unpack
