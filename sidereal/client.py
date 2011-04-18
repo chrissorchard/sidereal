4a7c57a6f93b72e53dd3517b226028d9fdd630df
Jack Edge <yellowbounder@gmail.com>
2011-04-18 16:15:33 +0100

    Beginnings of keyframe/diff handlers for client

--- sidereal/client.py
+++ sidereal/client.py
@@ -19,11 +19,26 @@ class Client(object):
 
         self.port = sidereal.network.DEFAULT_PORT
         self.handler = Handler()
+        self.protocol = sidereal.network.PacketReciever(self.handler)
+        self.manager = sidereal.network.PacketManager(self.protocol)
+    def gamestate_wrapper(self):
+        self.manager.check()
+        self.gamestate.tick()
 
 class Handler(sidereal.server.Handler):
     def __init__(self,client):
         self.client = client
 
         # the server.Handler method will check this variable
-        self.type_action = {}
+        self.type_action = {"snap":self.keyframe_handler}
+        self.type_action = {"diff":self.diff_handler}
+    def keyframe_handler(self,data,(host,port)):
+        client_time = self.client.gamestate.time
+        server_time = data['time']
+        gamestate = self.client.gamestate
+
+
+    def diff_handler(self,data,(host,port)):
+        time = self.client.gamestate.time
+        gamestate = self.client.gamestate
 
