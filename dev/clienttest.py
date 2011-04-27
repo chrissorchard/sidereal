acf426c62b63a78ff1b6f9f0aaf810a59012a34f
Jack Edge <yellowbounder@gmail.com>
2011-04-27 18:12:37 +0100

    Fixed reference to StdinInput class

--- dev/clienttest.py
+++ dev/clienttest.py
@@ -14,7 +14,7 @@ from twisted.protocols import basic
 HOST,PORT = "127.0.0.1",25005
 
 input = sidereal.network.StdinInput(c,(HOST,PORT))
-stdio.StandardIO(StdinInput(c,(HOST,PORT)))
+stdio.StandardIO(sidereal.network.StdinInput(c,(HOST,PORT)))
 
 from twisted.internet import reactor
 reactor.run()
